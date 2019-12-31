3f39a3ed2f8ad2281b05a6348c96b0cc52e06763
Tahsin <tahsingenuies@gmail.com>
2019-12-31 13:30:56 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRvarIdg5-Q3-FQYIS5dpLxNVnunUigx28md5Naiby9Tlzn1xvG"
+          alt=""
+        />
+        <h1>Hi, I am Tahsin</h1>
+        <h3>
+          I am currently developing this app with <b>React</b>.
+        </h3>
+        <div>
+          <p>For now visit the following:</p>
+          <ul>
+            <li>
+              <a href="https://github.com/tahsinature">Github</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/t4h51n">LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://linktr.ee/t4h51n">Linktree</a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/t4h51n">Facebook</a>
+            </li>
+            <li>
+              <a href="http://instagram.com/t4h51n">Instagram</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
