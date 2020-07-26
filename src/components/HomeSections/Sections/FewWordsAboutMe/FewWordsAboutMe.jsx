2e4f0dc94e3bc51c8ba982c8d81acd5ae759c5b4
Tahsin <tahsingenuies@gmail.com>
2020-07-26 20:47:33 +0700

    refactor: 💡 update few words about me

--- src/components/HomeSections/Sections/FewWordsAboutMe/FewWordsAboutMe.jsx
+++ src/components/HomeSections/Sections/FewWordsAboutMe/FewWordsAboutMe.jsx
@@ -4,9 +4,9 @@ const FewWordsAboutMe = props => {
   return (
     <section className="f4 lh-copy">
       <h2 className="f2 avenir ttu tracked bb-l tc">Few words about myself</h2>
-      <p style={{textAlign: 'justify'}}>
-        I'm a Jakarta based Software Engineer. A passionate and pragmatic programmer with 2 years of professional experience, specializing in full-stack development using modern & robust
-        technologies..
+      <p style={{ textAlign: 'justify' }}>
+        I'm a Jakarta based Software Engineer. A passionate and pragmatic programmer with 2 years of professional experience, specializing in full-stack development using modern & robust technologies.
+        Sometimes I work on things I find interesting, or things I think other people might find interesting. It’s nice when those things overlap.
       </p>
     </section>
   );
