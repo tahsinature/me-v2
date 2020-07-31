6eba0cab17374ea8652c2afcbca9eeeeab6649b6
Tahsin <tahsingenuies@gmail.com>
2020-07-31 14:02:43 +0700

    feat: 🎸 make list screen dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import {ThemeProvider} from 'styled-components';
-import {connect} from 'react-redux';
-import {Container} from '@material-ui/core';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { connect } from 'react-redux';
+import { Container } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Particles from 'react-particles-js';
 
 import './App.scss';
 import './assets/sass/main.scss';
 import classes from './App.module.scss';
-import {GlobalStyle} from './App.theme';
+import { GlobalStyle } from './App.theme';
 
 import NavBar from './components/NavBar/NavBar';
 
 import Home from './views/Home/Home';
 import Chat from './views/Chat/Chat';
 import Error404 from './views/Error404/Error404';
-import Tools from './views/Tools/Tools';
+import List from './views/List/List';
 
 import perticlesConfig from './perticles';
 
 function App(props) {
-  const {appState} = props;
+  const { appState } = props;
 
   return (
     <Router>
       <Particles className={classes.Particle} width="100vw" height="100vh" params={perticlesConfig} />
-      <ThemeProvider theme={{mode: appState.appTheme}}>
+      <ThemeProvider theme={{ mode: appState.appTheme }}>
         <GlobalStyle />
         <Container maxWidth="md">
           <div className={['App', classes.App].join(' ')}>
@@ -34,7 +34,7 @@ function App(props) {
               <Switch>
                 <Route exact path="/" component={() => <Home />} />
                 <Route path="/chat" component={() => <Chat />} />
-                <Route path="/tools" component={() => <Tools />} />
+                <Route path="/list" component={({ location }) => <List {...location.state} />} />
                 <Route component={() => <Error404 />} />
               </Switch>
             </div>
@@ -45,6 +45,6 @@ function App(props) {
   );
 }
 
-const mapStateToProps = state => ({appState: state.appReducer});
+const mapStateToProps = state => ({ appState: state.appReducer });
 
 export default connect(mapStateToProps, null)(App);
--- src/components/HomeSections/Sections/ToolsIAmUsing/ToolsIAmUsing.jsx
+++ src/components/HomeSections/Sections/ToolsIAmUsing/ToolsIAmUsing.jsx
@@ -1,6 +1,54 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
+const data = [
+  {
+    title: 'gRPC',
+    description: 'I like gRPC transport mechanism for request/response as non-persistent streaming protocol.',
+    image: 'https://s.appbrain.com/static/202003041452037/blob/sdk-logos/grpc.png',
+  },
+  {
+    title: 'Socket.IO',
+    description: 'I am using Socket.IO for bi-directional communication for quite a long now.',
+    image: 'https://firebasestorage.googleapis.com/v0/b/global-daf32.appspot.com/o/me%2Fsocket-io-logo.svg?alt=media&token=8c692e6c-a53e-4054-b19c-6d99c5d1e643',
+  },
+  {
+    title: 'FCM',
+    description: `Using Firebase Cloud Messaging (FCM) since 2019, as it's powered by Google, feels pretty consistant and reliable.`,
+    image: `https://firebasestorage.googleapis.com/v0/b/global-daf32.appspot.com/o/me%2Ffirebase_logo.png?alt=media&token=c1230b32-e464-48f0-8d3f-b49fe600f71b`,
+  },
+  {
+    title: 'Docker',
+    description: `Everyday's tool. In fact if I create a new repo now, I put it on docker first.`,
+    image: `https://firebasestorage.googleapis.com/v0/b/global-daf32.appspot.com/o/me%2Fdocker-logo.png?alt=media&token=531f83a7-ace5-4a31-b825-822dbc1aa6c5`,
+  },
+  // {
+  //   title: 'Kubernetes',
+  //   description: 'It's basically for DevOps. But sometimes we as backend engineer, need to ',
+  //   image: "It's basically for DevOps. But sometimes we as backend engineer, need to ",
+  // },
+  {
+    title: 'Jest',
+    description: `Previously I was using Jasmine for testing. Now using Jest for couple of years now.`,
+    image: `https://firebasestorage.googleapis.com/v0/b/global-daf32.appspot.com/o/me%2Fjest-logo.jpg?alt=media&token=a97a9453-7782-4a0f-bde4-915274ebbff9`,
+  },
+  {
+    title: 'Redux',
+    description: `If I use React or React Native apps, redux will take place there for sure.`,
+    image: `https://firebasestorage.googleapis.com/v0/b/global-daf32.appspot.com/o/me%2Fredux-logo.png?alt=media&token=2868fad7-92b8-46d3-be0c-fa1b6039b1d1`,
+  },
+  {
+    title: 'SonarQube',
+    description: `I use Sonar to inspect my code quality, detect bugs, code smells and security vulnerabilities. Really helps when I work in a team.`,
+    image: `https://firebasestorage.googleapis.com/v0/b/global-daf32.appspot.com/o/me%2Fsonarqube-logo.png?alt=media&token=c75b584c-77d4-4341-8c5b-71616300830d`,
+  },
+  {
+    title: 'HashiCorp Vault',
+    description: `It's a single source of truth to manage credentials. I use it for storing application API keys, passwords, certificates and other sensitive data.`,
+    image: `https://firebasestorage.googleapis.com/v0/b/global-daf32.appspot.com/o/me%2Fhashicorp-vault-logo.png?alt=media&token=950f4c59-a5b0-4eaf-8b1b-683c8889e4c4`,
+  },
+];
+
 const ToolsIAmUsing = props => {
   const history = useHistory();
 
@@ -8,67 +56,21 @@ const ToolsIAmUsing = props => {
     <section className="f4 lh-copy">
       <h2 className="f2 avenir ttu tracked bb-l tc">Tools I'm using nowadays</h2>
       <ul>
-        <li>
-          <a className="fw6 green no-underline underline-hover" href="https://www.google.com/">
-            gRPC
-          </a>
-          : I like gRPC transport mechanism for request/response as non-persistent streaming protocol.
-        </li>
-        <li>
-          <a className="fw6 green no-underline underline-hover" href="https://www.google.com/">
-            Socket.IO
-          </a>
-          : I am using Socket.IO for bi-directional communication for quite a long now.
-        </li>
-        <li>
-          <a className="fw6 green no-underline underline-hover" href="https://www.google.com/">
-            FCM
-          </a>
-          : Using Firebase Cloud Messaging (FCM) since 2019, as it's powered by Google, feels pretty consistant and reliable.
-        </li>
-        <li>
-          <a className="fw6 green no-underline underline-hover" href="https://www.google.com/">
-            Docker
-          </a>
-          : Everyday's tool. In fact if I create a new repo now, I put it on docker first.
-        </li>
-        {/* <li>
-          <a className="fw6 green no-underline underline-hover" href="https://www.google.com/">
-            Kubernetes
-          </a>
-          : It's basically for DevOps. But sometimes we as backend engineer, need to 
-        </li> */}
-        <li>
-          <a className="fw6 green no-underline underline-hover" href="https://www.google.com/">
-            Jest
-          </a>
-          : Previously I was using Jasmine for testing. Now using Jest for couple of years now.
-        </li>
-        <li>
-          <a className="fw6 green no-underline underline-hover" href="https://www.google.com/">
-            Redux
-          </a>
-          : If I use React or React Native apps, redux will take place there for sure.
-        </li>
-        <li>
-          <a className="fw6 green no-underline underline-hover" href="https://www.google.com/">
-            SonarQube
-          </a>
-          : I use Sonar to inspect my code quality, detect bugs, code smells and security vulnerabilities. Really helps when I work in a team.
-        </li>
-        <li>
-          <a className="fw6 green no-underline underline-hover" href="https://www.google.com/">
-            HashiCorp Vault
-          </a>
-          : It's a single source of truth to manage credentials. I use it for storing application API keys, passwords, certificates and other sensitive data.
-        </li>
+        {data.map(ele => (
+          <li key={ele.title}>
+            <a className="fw6 green no-underline underline-hover" href="https://www.google.com/">
+              {ele.title}
+            </a>
+            : {ele.description}
+          </li>
+        ))}
         <li>
           <a
             className="fw6 green no-underline underline-hover"
             href="/tools"
             onClick={e => {
               e.preventDefault();
-              history.push('/tools');
+              history.push('/list/tools', { data, listTitle: `List of tools I've been using` });
             }}>
             See All...
           </a>
