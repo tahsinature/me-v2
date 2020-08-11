f4694764978aa60c6af3d67d0547aca1cc42e03a
Tahsin <tahsingenuies@gmail.com>
2020-08-11 21:04:57 +0700

    refactor: 💡 scroller func

--- src/views/Chat/Chat.jsx
+++ src/views/Chat/Chat.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, createRef } from 'react';
+import React, { useEffect } from 'react';
+
 // RCE CSS
 import 'react-chat-elements/dist/main.css';
 // MessageBox component
 import { MessageBox, MessageList } from 'react-chat-elements';
 import _ from 'lodash';
 
-// import './Chat.scss';
+import './Chat.scss';
 
 import classes from './Chat.module.scss';
+import appClasses from '../../App.module.scss';
 import Conversation from './Conversation/Conversation';
 import { Root, Header } from './Chat.theme';
 import faker from 'faker';
@@ -22,23 +24,40 @@ import faker from 'faker';
 // };
 
 const Chat = props => {
-  useEffect(() => {});
+  useEffect(() => {
+    console.log(appClasses);
+
+    // const container = document.querySelector();
+    // setInterval(() => {
+    //   ps.top`
+    // }, 2000);
+    // ps.update();
+  });
 
   return (
     <Root className={classes.Chat}>
-      <MessageList
-        className="message-list"
-        lockable={true}
-        toBottomHeight={'100%'}
-        dataSource={Array.from({ length: 5 }, () => {
-          return {
-            position: _.sample(['left', 'right']),
-            type: 'text',
-            text: faker.random.words(),
-            date: Date.parse(faker.date.past()),
-          };
-        })}
-      />
+      <div>
+        <div style={{ padding: '1rem', backgroundColor: 'yellow' }}>
+          {Array(50)
+            .fill(faker.random.words(5))
+            .map(word => (
+              <p>{word}</p>
+            ))}
+        </div>
+        {/* <MessageList
+          className="message-list"
+          lockable={true}
+          toBottomHeight={'100%'}
+          dataSource={Array.from({ length: 50 }, () => {
+            return {
+              position: _.sample(['left', 'right']),
+              type: 'text',
+              text: faker.random.words(),
+              date: Date.parse(faker.date.past()),
+            };
+          })}
+        /> */}
+      </div>
       {/* <Header className={classes.RecieverHeader}>
         <h4>Mohammad Tahsin</h4>
         {getOnLineStatusComponent()}
